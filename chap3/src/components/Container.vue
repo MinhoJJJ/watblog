8f4f948b5409f8d3225bad0472440f93e8fb42d5
DESKTOP-SFM2B1D\JUNGMINHO <jmh2023@aspncloud.com>
2023-08-02 19:51:28 +0900

    하위하위프롭스

---
+++ chap3/src/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    gd
+    <Post/>
+    <Post/>
+    <Post :insta="insta"/>
+    {{insta.name[0]}}
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+    name: 'Container',
+    components: {
+    Post: Post,
+  },
+  props:{
+      insta:Array,
+  },
+}
+</script>
+
+<style>
+
+</style>
